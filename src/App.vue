82dd0c2dbfe0029da83844d47a0fbf6749493de6
kohei <bhkbb0551@gmail.com>
2022-12-04 00:19:10 +0900

    [feat] v-bindの使い方'

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- // v-bind: このタブのコンポーネントにデータを渡す。 
+    v-bind:todos='todos' で、 todosという属性でtodosの値を渡す。 -->
+  <TodosVue v-bind:todos="todos"></TodosVue>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodosVue from './components/Todos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TodosVue
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'todo one',
+          completed: false,
+        },
+        {
+          id: 2,
+          title: 'todo two',
+          completed: true,
+        },
+        {
+          id: 3,
+          title: 'todo three',
+          completed: false,
+        },
+      ]
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>

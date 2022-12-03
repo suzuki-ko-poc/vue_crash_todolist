9b76b5144aa307fcb718970745b32cd33d37cc0c
kohei <bhkbb0551@gmail.com>
2022-12-04 00:19:54 +0900

    [feat] v-forの使い方

---
+++ src/components/Todos.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <!-- // v-bind:key は :keyと省略出来る。 -->
+        <div v-for="todo in todos" :key="todo.id">
+            <h2>{{todo.title}}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Todos',
+    components: {
+    },
+    // QUESTION なぜv-bindで渡したものを propsで持つのだろうか？
+    props: ["todos"]
+
+}
+</script>
+
+<style scoped>
+
+</style>

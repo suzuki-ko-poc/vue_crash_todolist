2db42828c92fa4537c8f0c72359981007a6feccb
kohei <bhkbb0551@gmail.com>
2022-12-04 20:35:36 +0900

    [feat] "v-modelの使い方"と"dataとpropsの違い"

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,17 +1,47 @@
 <template>
-    <div>
-        <form>
-            <input type="text" name="title" placeholder="AddTodo...">
-            <input type="submit" value="Submit" class="btn"> 
-        </form>
-    </div>
-
+  <div>
+    <form @submit="addTodo">
+      <!-- v-modelはv-onとv-bindをまとめて一行で書ける。 -->
+      <input
+        type="text"
+        v-model="title"
+        name="title"
+        placeholder="AddTodo..."
+      />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
 </template>
 
 
 <script>
+import uuid from "uuid";
+
 export default {
   name: "AddTodo",
+  /**
+   * dataとpropsの違いは、
+   * props: 親コンポーネント(外側)から渡されるであろう値を自コンポーネントに定義しておくもの。
+   * data: コンポーネントがインスタンス化される時に生成される。コンポーネントが持っているコンストラクタみたいなもんかな？？
+   * 
+   * propsは、this.XXX みたいなアクセスはできないけど、 dataは this.XXXみたいなアクセスができるってことかな？
+   * */
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      const newTodo = {
+        id: uuid.v4(),
+        title: this.title,
+        complated: false,
+      };
+
+      this.$emit("add-todo", newTodo);
+    },
+  },
 };
 </script>
 
